e28a9cc8bbb5a542b88e1b9eab62e45ad30c02bb
piekarski-mateusz <mateusz.piekarski@iceye.fi>
2026-01-13 17:22:41 +0100

    Inicjalizacja Django

---
+++ pakernia/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+    
+    # API
+    path('api/', include('api.urls')),
+    
+    # Swagger 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
